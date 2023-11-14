313da97ed3c944a4a355f7d30d3cdc12850bc85b
fluidjeel <mmanasjit@gmail.com>
2023-11-14 18:02:45 +0530

    folder str

---
+++ eip/eip-sg.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+locals {
+  tags = {
+    Name = "plt-tf-test-eip"
+    AppName = "terra-eip-test"
+    Backup = "no"
+    Environment = "poc"
+    DataClassification = "internal"
+    InfraOwner = "sre-cloud-reliability@tavisca.com"
+    Product = "plt"
+    BusinessUnit = "travel.poc"
+
+  }
+}
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+  tags = local.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_eip.lb.public_ip]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
